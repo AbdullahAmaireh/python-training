a959c89895a516630b4d15229ea5dd27d1f7a55e
AbdullahAmaireh <112310853+AbdullahAmaireh@users.noreply.github.com>
2022-09-20 01:56:08 +0300

    Python

---
+++ train.day.1.py
@@ -0,0 +1,138 @@
+""" print("abdullah")
+
+print('o---')
+
+print((" ||| "))
+
+print('*' * 10) 
+
+price = 20
+rating = 4.4
+name = 'mosh'
+is_published = False
+print( price )"""
+
+#the comand between "" is name (string)
+# we calling 4.4 for exapmle (float) num
+# true and false is (boolean)
+# we calling the list and object (complex) 
+# we calling the clear number like 20 or 10 or 5 or 3 (integer)
+
+
+"""full_name = 'john smith' #str
+age = 20 #float
+is_new = True #poly 
+
+name =input('what is your name')
+print('hi ' , name) 
+
+person_name = input('What is your name ? ')
+favourit_color = input('what is your favourite color ?')
+
+print('hi' + person_name )
+print('mmmm i think' , favourit_color , 'is nice color') 
+"""
+
+# int()
+# float()
+# bool()
+
+
+"""
+birth_year = input('birth year')
+print(type(birth_year))
+age = 2022 - int(birth_year)
+print(type(age))
+print(age) 
+
+
+weight_lb = input("your weight in lb pleas")
+weight_kg = 0.45 * int(weight_lb)
+print("your weight in kg is" , weight_kg) 
+
+course = '''
+hi abdullah
+how are you today 
+i hope you are ok 
+'''
+print(course)
+
+course = 'python for beginners'
+          #012345 678 9
+another = course[:]
+print(another)
+
+name = 'jennifer'
+print(name[1:-1])
+
+first = 'john'
+last = 'smith'
+message = first + '[' + last + '] is a coder '
+msg = f'{first}  [{last}] is a coder'
+print(msg)
+print(message)"""
+
+"""course = ' Pytho for beginners '
+print(course.replace('b' , "j"))
+print('Pytho' in course)
+"""
+
+
+# course.replace
+# course.fined
+# course.upper
+# course.lower
+# course.title
+# len()
+# 'write any thing...' in course (to find word in the commant)
+# dont forget to use(use .)
+
+
+"""print(10 ** 7)
+
+x = 10
+x = 10 + 3
+print(x)
+
+y = 10
+y -=3
+print(y)"""
+
+"""x = 10 + 3 * 2
+print(x)"""
+ 
+
+# try to use / and //
+# try to use * and **
+# try to use %
+
+
+"""
+x = (10 + 3) * 2 + 2**4
+print(x)
+"""
+
+
+# The Arithmetic priority
+# parenthesis الاقواس ()
+# exponentiation for example (2**3)
+# multiplication (*) or division (/)
+# addition (+) subtraction (-)
+
+
+""""
+x = 2.9
+print(round(x))
+
+y=-4.5
+print(abs(y))
+"""
+"""
+import math
+
+print(math.ceil(7.5))
+
+print(math.floor(3.7))
+"""
+
+# search on google about ( python 3 math module) to know more about the function
